a5e2c05ff0f25f39116aada6c68391ad8c29a5bc
Nehemiah Reese <Jefe@nehemiahs-air.attlocal.net>
2016-06-30 19:20:03 -0400

    Initial Commit

---
+++ PushUP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushUP
+//
+//  Created by Nehemiah Reese on 6/30/16.
+//  Copyright © 2016 Nehemiah Reese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
