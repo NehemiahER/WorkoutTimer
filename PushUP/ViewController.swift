14e3a0f9ae0146f6144ef12c30f9d6d6377fdc48
Nehemiah Reese <Jefe@nehemiahs-air.attlocal.net>
2016-08-03 16:40:10 -0400

    New project (1st upload)

--- PushUP/ViewController.swift
+++ PushUP/ViewController.swift
@@ -10,15 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    var timer: NSTimer?
+    
+    var accumulator = 0
+    
+    @IBOutlet weak var labelForCounter: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.labelForCounter?.text = "Hardwork and Dedication"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    @IBAction func pushUpTapped(sender: AnyObject) {
+        self.labelForCounter?.text = "Start your work out!"
+        if (timer == nil) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("update") , userInfo: nil, repeats: true)
+        }
+    }
+
+    @IBAction func resetCounter(sender: AnyObject) {
+        accumulator = 0
+        counterLabel.text = "\(accumulator)"
+        if timer != nil {
+            timer!.invalidate()
+            timer = nil
+        }
+     labelForCounter?.text = "Tap to start your work out!"
+
+    }
+    
+    func update() {
+        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+            self.accumulator += 1
+            self.counterLabel.text = "\(self.accumulator)"
+        }
+    }
+
+    @IBAction func stopButtonPressed(sender: AnyObject) {
+        timer?.invalidate()
+        
+        switch (self.accumulator) {
+        case 0...10: self.labelForCounter?.text = "Keep up the great work!"
+        case 11...30: self.labelForCounter?.text = "Your a titan!!!"
+        case 40...100: self.labelForCounter!.text = "Beast Mode"
+        default: self.labelForCounter?.text = "Your a different animal 💪!!!"
+        
+        }
+      
+        
+    }
+
+
 
 
 }
